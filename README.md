fce05ff3cf9f85f352b3e054b100b5f6048331c7
Vitor Leonel <falecom@vitorleonel.com>
2019-03-17 16:50:51 -0300

    start project

---
+++ README.md
@@ -0,0 +1,7 @@
+# OS Agent
+
+### How to install agent
+
+```sh
+export DEBIAN_FRONTEND=noninteractive; apt-get update; apt-get install curl -y; curl https://raw.githubusercontent.com/osagent/installer/master/installer.sh | sh; export DEBIAN_FRONTEND=newt;
+```
---
+++ installer.sh
@@ -0,0 +1,35 @@
+function BootstrapInstaller() {
+  # General packages
+  apt install git -y
+
+  # Node.Js LTS version
+  curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+  apt install nodejs - y
+}
+
+function BootstrapAgent() {
+  # Clone agent project
+  git clone git@github.com:osagent/agent.git /home/agent
+
+  # Navite to agent path
+  cd /home/agent
+
+  # Install dependencies
+  npm install
+}
+
+function BootstrapSystemdService() {
+
+}
+
+# Bootstrap the installer
+sleep 2
+BootstrapInstaller
+
+# Agent
+sleep 2
+BootstrapAgent
+
+# Systemd Service
+sleep 2
+BootstrapSystemdService
